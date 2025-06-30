80fd6798d6fbf51b5d7482582c516bbfbf4c2c72
murraysean <murraysean@microsoft.com>
2025-06-30 13:42:19 -0700

    Inject env var in GH workflow

--- infra/scripts/agents/run_agents_setup.sh
+++ infra/scripts/agents/run_agents_setup.sh
@@ -9,9 +9,6 @@ cd ${script_dir}
 
 echo "Running agents setup..."
 
-echo "inject val: ${TRIAGE_AGENT_INJECT_EXAMPLES}"
-echo "wf val: ${IS_GITHUB_WORKFLOW_RUN}"
-
 export ENV_FILE="${script_dir}/../.env"
 python3 -m pip install -r requirements.txt
 python3 agents_setup.py
