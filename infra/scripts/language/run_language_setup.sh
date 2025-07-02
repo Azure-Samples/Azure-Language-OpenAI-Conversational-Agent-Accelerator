32cb0927bae4790906708cea7e6e6844b4f55fdd
murraysean <murraysean@microsoft.com>
2025-07-02 11:37:08 -0700

    Add CQA skip setup option

--- infra/scripts/language/run_language_setup.sh
+++ infra/scripts/language/run_language_setup.sh
@@ -14,7 +14,9 @@ cp ../data/*.json .
 
 python3 -m pip install -r requirements.txt
 python3 clu_setup.py
-python3 cqa_setup.py
+if [ "$SKIP_CQA_SETUP" != "true" ]; then
+    python3 cqa_setup.py
+fi
 python3 orchestration_setup.py
 
 # Cleanup:
